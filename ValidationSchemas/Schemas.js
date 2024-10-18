05661dfa4bb0227fbf917443dd21791c74ab0a68
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2024-10-18 22:49:40 +0530

    Update Schemas.js

--- ValidationSchemas/Schemas.js
+++ ValidationSchemas/Schemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const schemas ={
     register: {
         body: Joi.object({
-            name: Joi.string().min(3).max(20).required(),
+            name: Joi.string().min(3).required(),
             email: Joi.string().email().required(),
             password: Joi.string().min(6).required(),
             }),
